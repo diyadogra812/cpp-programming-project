dea6e573d46fc934c7be9e0541446331d1ae83d5
Diya Dogra <diyadogra812@gmail.com>
2026-04-28 00:28:52 +0530

    Add files via upload

---
+++ 21st.cpp
@@ -0,0 +1,54 @@
+21. WAP to demonstrate constructor overloading 
+
+// #include <iostream>
+// using namespace std;
+
+// class Rectangle {
+// private:
+//     int length;
+//     int breadth;
+
+// public:
+//     
+//     Rectangle() {
+//         length = 0;
+//         breadth = 0;
+//     }
+
+    
+//     Rectangle(int side) {
+//         length = side;
+//         breadth = side;
+        
+//     }
+
+    
+//     Rectangle(int l, int b) {
+//         length = l;
+//         breadth = b;
+       
+//     }
+
+//     void display() {
+//         cout << "Length: " << length << endl;
+//         cout << "Breadth: " << breadth << endl;
+//         cout << "Area: " << length * breadth << endl;
+//        
+//     }
+// };
+
+// int main() {
+
+//     Rectangle r1;     
+//     r1.display();
+
+//     Rectangle r2(5);       
+//     r2.display();
+
+//     Rectangle r3(4, 6);   
+//     r3.display();
+
+//     return 0;
+// }
+
+
